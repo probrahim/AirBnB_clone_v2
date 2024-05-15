4094be1a340464a777bb4d8bf6e3fe0eb0c747ee
probrahim <ibrahimhassani674@gmail.com>
2024-05-15 20:14:14 +0200

    task 01

---
+++ 1-pack_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+import os.path
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
+
+
+def notify_pack_creation(file):
+    """Notify the user about the successful creation of the pack."""
+    print("Archive {} created successfully!".format(file))
